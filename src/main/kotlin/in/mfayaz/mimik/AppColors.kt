96e38744290b885b97416846eb8767fb40963986
Mohammad Fayaz <fayaz.mohammad@thoughtworks.com>
2024-10-04 21:50:07 +0530

    add simple screen code

---
+++ src/main/kotlin/in/mfayaz/mimik/AppColors.kt
@@ -0,0 +1,28 @@
+package `in`.mfayaz.mimik
+
+import javafx.scene.paint.Color
+
+object AppColors {
+  val primary = Color.web("0xFF5271FF")
+  val secondary = Color.web("0xFF52C7FF")
+  val accent = Color.web("0xFFFF52D4")
+  val secondaryDark = Color.web("0xFF118bc6")
+  val secondaryLight = Color.web("0xFFc3ebff")
+
+  val neutralLight = Color.web("0xFFF0F2F5")
+  val neutralMedium = Color.web("0xFFD1D5DB")
+  val neutralDark = Color.web("0xFF4B5563")
+
+  val textPrimary = Color.web("0xFF1F2937")
+  val textSecondary = Color.web("0xFF6B7280")
+
+  val backgroundMain = Color.web("0xFFFFFFFF")
+  val backgroundSecondary = Color.web("0xFFF9FAFB")
+
+  val primaryLight = Color.web("0xFF7A91FF")
+  val primaryDark = Color.web("0xFF3955D6")
+
+  val success = Color.web("0xFF10B981")
+  val warning = Color.web("0xFFFBBF24")
+  val error = Color.web("0xFFEF4444")
+}
--- src/main/kotlin/in/mfayaz/mimik/HelloApplication.kt
+++ src/main/kotlin/in/mfayaz/mimik/HelloApplication.kt
@@ -1,16 +1,37 @@
 package `in`.mfayaz.mimik
 
 import javafx.application.Application
-import javafx.fxml.FXMLLoader
+import javafx.geometry.Rectangle2D
 import javafx.scene.Scene
+import javafx.scene.layout.StackPane
+import javafx.scene.text.Text
+import javafx.stage.Screen
 import javafx.stage.Stage
 
 class HelloApplication : Application() {
+
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
-        stage.title = "Hello!"
+// Get the dimensions of the primary screen
+        val screenBounds: Rectangle2D = Screen.getPrimary().bounds
+        val width = screenBounds.width * 0.7 // 70% of screen width
+        val height = screenBounds.height * 0.7 // 70% of screen height
+
+        // Create a simple layout
+        val root = StackPane()
+        val scene = Scene(root, width, height)
+
+        // create a text object and add it to the layout
+        val textComponent = Text("Hello")
+        root.children.add(textComponent)
+
+        // Set the title and scene of the primary stage
+        stage.title = "70% Screen Size Example"
         stage.scene = scene
+
+        // Center the stage on the screen
+        stage.x = (screenBounds.width - width) / 2
+        stage.y = (screenBounds.height - height) / 2
+
         stage.show()
     }
 }
