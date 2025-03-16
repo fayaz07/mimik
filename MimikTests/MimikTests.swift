d9948e72b22c8e7b711d26434708d80b7f222e56
Mohammad Fayaz <fayazfz07@gmail.com>
2025-03-16 12:39:26 +0530

    hello swift 👋🏻 - there's no going back now

---
+++ MimikTests/MimikTests.swift
@@ -0,0 +1,17 @@
+//
+//  MimikTests.swift
+//  MimikTests
+//
+//  Created by Mohammad Fayaz on 16/03/25.
+//
+
+import Testing
+@testable import Mimik
+
+struct MimikTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
