096e3c714f6aeb2194d2d7ddaa56b3ead7fa2381
Mohammad Fayaz <35001172+fayaz07@users.noreply.github.com>
2025-04-10 17:41:46 +0530

    [wip] add Workspace entity

---
+++ Mimik/Data/Models/WorkspaceEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  WorkspaceEntity+CoreDataClass.swift
+//  Mimik
+//
+//  Created by Mohammad Fayaz on 31/03/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(WorkspaceEntity)
+public class WorkspaceEntity: NSManagedObject {
+
+}
--- Mimik/UI/Workspace/WorkspacesView.swift
+++ Mimik/UI/Workspace/WorkspacesView.swift
@@ -16,3 +16,30 @@ struct WorkspacesView: View {
 #Preview {
     WorkspacesView()
 }
+
+//
+//  WorkspaceModel.swift
+//  Mimik
+//
+//  Created by Mohammad Fayaz on 25/03/25.
+//
+
+//import SwiftUI
+//import SwiftData
+//
+//@Model
+//final class WorkspaceModel {
+//  @Attribute(.unique) var id: UUID = UUID()
+//  
+//  var name: String = ""
+//  var deleted: Bool = false
+//  var lastAccessed: Date = Date()
+//  var created: Date = Date()
+//  
+//  init(name: String, deleted: Bool, lastAccessed: Date, created: Date) {
+//    self.name = name
+//    self.deleted = deleted
+//    self.lastAccessed = lastAccessed
+//    self.created = created
+//  }
+//}
